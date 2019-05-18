b4a84757d1496b8eeae0234f644f775f481641e1
Philip Andes <philip__andes@hotmail.nl>
2019-05-18 17:56:36 +0200

    Add Item To Data Structure

--- app.js
+++ app.js
@@ -34,6 +34,31 @@ const ItemCtrl = (function() {
             // All we doing here is:
             return data.items;
         },
+        addItem: function(name, calories){
+            let ID;
+            // We need to generate a ID:
+            // if data.items is greater then 0
+            if(data.items.length > 0){
+                // Then we want the index to be the length minus 1, so if there is 4 we want this to be 3 etc
+                ID = data.items[data.items.length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            // Then we want to parse the calories as a number, because it is going to be string now:
+            calories = parseInt(calories);
+
+            // Then we want to create a new item
+            // Remember we have a constructor in the Item controller so we have to say new Item and pass in the data:
+            newItem = new Item(ID, name, calories);
+
+            // Then we want to push the new item to the data structure in the item controller:
+            // Add to items array:
+            data.items.push(newItem);
+
+            // And then we simply going to return the new item because remember we going to put it in a variable down below!
+            return newItem;
+
+        },
         logData: function(){
             return data;
         }
@@ -47,8 +72,12 @@ const UICtrl = (function() {
     // the #item-list can be changed at any time, and we dont want to go through all our javascript and change it each indivitual one 
     // So what we do, is create a object called UI selectors
     // Any class or id we use inside of a selector we want to put inside here:
+    // It makes the code more efficient as wel as more scalable
     const UISelectors = {
-        itemList: '#item-list'
+        itemList: '#item-list',
+        addBtn: '.add-btn',
+        itemNameInput: '#item-name',
+        itemCaloriesInput: '#item-calories'
     }
 
     // Public methods:
@@ -76,6 +105,21 @@ const UICtrl = (function() {
             // So select the id from the DOM and change it to the variable html from above
             // See the UISelector object above for more info
             document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        // Get item input 
+        
+        getItemInput: function(){
+            return {
+                // we can get it by the id's we gave it in the UI, so we first have to add those at our UISelectors again
+                // Return a object with the name and calories
+                // since it is an input we have to use .value
+                name:document.querySelector(UISelectors.itemNameInput).value,
+                calories:document.querySelector(UISelectors.itemCaloriesInput).value
+            }
+        },
+        // To use UISelectors we first have to make it public:
+        getSelectors: function(){
+            return UISelectors;
         }
     }  
 })();
@@ -84,6 +128,34 @@ const UICtrl = (function() {
 
 // App Controller 
 const App = (function(ItemCtrl, UICtrl) {
+    // Load event listeners
+    const loadEventListeners = function(){
+        // We have to call the loadEventListeners in the init function below!
+        // Get UI Selectors:
+        const UISelectors = UICtrl.getSelectors();
+
+        // We need an event to add a item:
+        document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+    }
+
+    // Add item submit:
+    const itemAddSubmit = function(e){
+        // Check if there something here:
+        // Get form input from UI Controller:
+        const input = UICtrl.getItemInput();
+
+        // If either name or calories is not filled, the user can't add anything!
+        // Check for name and calorie input:
+        // so if name input is not empty AND calories input is not empty we can proceed:
+        if(input.name !== '' && input.calories !== ''){
+            // Add item 
+            const newItem = ItemCtrl.addItem(input.name, input.calories);
+        }
+
+        e.preventDefault();
+    }
+
+
     // Public methods:
     // We want the app controller to return just one function or one method and that is going to be the init. (the initializer for the app)
     // Everything we need to run right away when the application loads thats gonna go
@@ -101,6 +173,9 @@ const App = (function(ItemCtrl, UICtrl) {
             // Populate list with items:
             // So we gonna have  a UI function or method, and we going to pass in the items we fetch above
             UICtrl.populateItemList(items);
+
+            // Load event listeners:
+            loadEventListeners();
         }
     }
     
