eeb790e36af140e6c834e38fb69efda6cfb882f8
Philip Andes <philip__andes@hotmail.nl>
2019-05-18 17:18:11 +0200

    Get & Populate Items

--- app.js
+++ app.js
@@ -29,21 +29,58 @@ const ItemCtrl = (function() {
 
     // Public methods:
     return {
+        // Let create a function getItems:
+        getItems: function(){
+            // All we doing here is:
+            return data.items;
+        },
         logData: function(){
             return data;
         }
     }
 })();
 
+
+
 // UI Controller
-const UICtrl = (function() {
+const UICtrl = (function() {        
+    // the #item-list can be changed at any time, and we dont want to go through all our javascript and change it each indivitual one 
+    // So what we do, is create a object called UI selectors
+    // Any class or id we use inside of a selector we want to put inside here:
+    const UISelectors = {
+        itemList: '#item-list'
+    }
 
     // Public methods:
     return {
-
-    }
-    
+        populateItemList(items){
+            // We want to loop through the items, and make each one into a list item <li> and insert it into the <ul>
+            // So lets define a variable to html and set it to nothing:
+            let html = '';
+            // Then take the items and loop through them:
+            // And remember a forEach takes in a callback function:
+            // Give it an indentifier (item)
+            items.forEach(function(item){
+                // Then lets take that html variable from above and append to it:
+                // We are going to have a <li> and we it to have a id, which would be item- the actual id of the item so that is going to be a variable:
+                // It is also going to have a class
+                // So everytime it loops true an <li> will  be added to the html variable above
+                html += `<li class="collection-item" id="item-${item.id}">
+                <strong>${item.name}: </strong> <em>${item.calories}  Calories</em>
+                <a href="#" class="secondary-content">
+                    <i class="edit-item fa fa-pencil"></i>
+                </a>
+            </li>`;
+            });
+            // So now we want to take all these <li>'s and put them into the <ul> in the application
+            // So select the id from the DOM and change it to the variable html from above
+            // See the UISelector object above for more info
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        }
+    }  
 })();
+
+
 
 // App Controller 
 const App = (function(ItemCtrl, UICtrl) {
@@ -55,7 +92,15 @@ const App = (function(ItemCtrl, UICtrl) {
     // So as you can see, all these modules are going to work very close together
     return {
         init: function(){
-            console.log('Initializing App....');
+            // Fetch items from data structure
+            // We want the getItem when the application initializes so we have to call it here:
+            // We are going to put the result of that in a variable:
+            const items = ItemCtrl.getItems();
+            // Remember the UI controller is going to take care of anything what is taking place on the UI, and populating the list on the UI with our items
+            // So what we want to do now is take the items we get from the item controller:
+            // Populate list with items:
+            // So we gonna have  a UI function or method, and we going to pass in the items we fetch above
+            UICtrl.populateItemList(items);
         }
     }
     
